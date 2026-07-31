a670af081013ca7dd9abc590a2ce357eeb05d4d7
Vishwas Shukla <vishwasshukla0507@gmail.com>
2026-07-31 13:13:48 +0530

    Initial Commit

---
+++ Router/RouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterApp.swift
+//  Router
+//
+//  Created by Vishwas Shukla on 31/07/26.
+//
+
+import SwiftUI
+
+@main
+struct RouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
