c6b08e043103c31375618eb0236ee93558fdeea2
Vishwas Shukla <vishwasshukla0507@gmail.com>
2026-07-31 17:45:56 +0530

    Router implementation added

--- Router/Router.swift
+++ Router/Router.swift
@@ -5,4 +5,35 @@
 //  Created by Vishwas Shukla on 31/07/26.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+/// A generic router for managing navigation between different screens in SwiftUI
+protocol NavigationDestination {
+    associatedtype Destination: View
+    
+    var title: String { get }
+    @ViewBuilder
+    var destinationView: Destination { get }
+}
+
+final class Router<Destination: NavigationDestination>: ObservableObject {
+    
+    /// Holds the stack of destinations for navigation
+    @Published var navPaths: [Destination] = []
+    
+    func navigate(to destination: Destination) {
+        navPaths.append(destination)
+    }
+    
+    func navigateBack() {
+        guard !navPaths.isEmpty else { return }
+        navPaths.removeLast()
+    }
+    
+    func navigateToRoot() {
+        navPaths.removeLast(navPaths.count)
+    }
+    
+}
+
